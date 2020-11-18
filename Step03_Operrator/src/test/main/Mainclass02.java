42a0825f71d7c842673e5c6a3ea39b67446cb501
kingura <oli999@naver.com>
2020-11-18 15:14:48 +0900

    commit

---
+++ Step03_Operrator/src/test/main/Mainclass02.java
@@ -0,0 +1,34 @@
+package test.main;
+/*
+ * 2. 증감 연산자 테스트
+ * 변수에 있는 숫자값을 1씩 증가 혹은 1씩 감소 시킬때 사용한다.
+ * ++, --
+ */
+public class Mainclass02 {
+	public static void main(String[] args) {
+		int num=0;
+		num++;
+		num++;
+		num++; // num 이 최종적으로 3이 된다.
+		
+		int num2=0;
+		num2--;
+		num2--;
+		num2--; //num이 최종적으로 -3이 된다.
+		
+		int num3=0;
+		int result1=++num3; // result1 은 1 num3 는 1
+		
+		int num4=0;
+		//변수 뒤에 있는 증감 연산자는 연산의 우선순위가 가장 늦다(대입연산자 보다도 늦다)
+		int result2=num4++; // result2 와 num4 값을 확인해 보세요.
+		
+		//num, num2 에있는 값을0으로 수정하고 다시 테스트
+		num=0;
+		num2=0;
+		
+		int result3=num-- + ++num2;
+		
+		System.out.println("mian 메소드가 종료 됩니다.");
+	}
+}
